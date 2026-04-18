7731516d0a4c0eb7208dfc99d26870f0735b59ed
yassir laaouisset <yassir@192.168.1.24>
2026-04-18 21:55:23 +0100

    feat: refactore dashboard header

--- client/src/shared/components/DashboardHeader.jsx
+++ client/src/shared/components/DashboardHeader.jsx
@@ -1,12 +1,20 @@
-import styles from '../styles/DashboardHeader.module.css'
+import styles from "../styles/DashboardHeader.module.css";
 
 function DashboardHeader() {
-    return (
-        <header className={styles.header}>
-            <h4 className={styles.fullName}>Full name</h4>
-            <p className={styles.email}>yassir@gmail.com</p>
-        </header>
-    )
+  return (
+    <header className={styles.header}>
+      <div className={styles.identity}>
+        <div className={styles.avatar}>FN</div>
+        <div className={styles.content}>
+          <span className={styles.dashboardOverViewTitle}>
+            Dashboard overview
+          </span>
+          <h4 className={styles.fullName}>Full name</h4>
+          <p className={styles.email}>yassir@gmail.com</p>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default DashboardHeader;
